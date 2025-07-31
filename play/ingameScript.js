0d225416c65ff5e1037164f1088d9de86e5b54c4
Olive :3 <141564336+vOlive-io@users.noreply.github.com>
2025-07-31 10:49:07 -0600

    Update ingameScript.js

--- play/ingameScript.js
+++ play/ingameScript.js
@@ -19,7 +19,17 @@ var Seeds = [
     ["2_1", "Seed Name", false],
     ["2_2", "Seed Name", false],
     ["2_2", "Seed Name", false]
-  ]]
+  ]],
+  ["Uncommon Seeds", [
+    ["3_1", "Seed Name", false],
+    ["3_2", "Seed Name", false],
+    ["3_2", "Seed Name", false]
+  ]],
+  ["Rare Seeds", [
+    ["4_1", "Seed Name", false],
+    ["4_2", "Seed Name", false],
+    ["4_2", "Seed Name", false]
+  ]],
 ];
 
 
