312d1d3717ae16121999609a6f9a4fe54256b0e7
Olive :3 <141564336+vOlive-io@users.noreply.github.com>
2025-07-31 10:59:09 -0600

    Update ingameScript.js

--- play/ingameScript.js
+++ play/ingameScript.js
@@ -12,24 +12,24 @@
 //Seeds array counts plants AND seeds in inventory
 var Seeds = [
   ["Starter Seeds", [
-    ["1.1.1", "Seed Name", "Seed Description", 5, 0, 1.00, true],
-    ["1.1.2", "Seed Name", "Seed Description", 5, 0, 1.50, true],
-    ["1.1.2", "Seed Name", "Seed Description", 5, 0, 2.00,true]
+    ["1.1.1", "Seed Name", "Seed Description", 5, 0, 1.00, 0, true],
+    ["1.1.2", "Seed Name", "Seed Description", 5, 0, 1.50, 0, true],
+    ["1.1.2", "Seed Name", "Seed Description", 5, 0, 2.00, 0, true]
   ]], 
   ["Common Seeds", [
-    ["1.2.1", "Seed Name", "Seed Description", false],
-    ["1.2.2", "Seed Name", "Seed Description", false],
-    ["1.2.2", "Seed Name", "Seed Description", false]
+    ["1.2.1", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false],
+    ["1.2.2", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false],
+    ["1.2.2", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false]
   ]],
   ["Uncommon Seeds", [
-    ["1.3.1", "Seed Name", "Seed Description", false],
-    ["1.3.2", "Seed Name", "Seed Description", false],
-    ["1.3.2", "Seed Name", "Seed Description", false]
+    ["1.3.1", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false],
+    ["1.3.2", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false],
+    ["1.3.2", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false]
   ]],
   ["Rare Seeds", [
-    ["1.4.1", "Seed Name", "Seed Description", false],
-    ["1.4.2", "Seed Name", "Seed Description", false],
-    ["1.4.2", "Seed Name", "Seed Description", false]
+    ["1.4.1", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false],
+    ["1.4.2", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false],
+    ["1.4.2", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false]
   ]],
 ];
 
