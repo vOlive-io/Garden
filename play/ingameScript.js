b93fa2e736f6e6a81ed51814d6ff953af999e24c
Olive :3 <141564336+vOlive-io@users.noreply.github.com>
2025-07-31 11:09:07 -0600

    Update ingameScript.js

--- play/ingameScript.js
+++ play/ingameScript.js
@@ -16,30 +16,30 @@
 var Seeds = [
   ["Starter Seeds", [
     ["1.1.1.1", "Seed Name", "Seed Description", 5, 0, 1.00, 0, true],
-    ["1.1.2", "Seed Name", "Seed Description", 5, 0, 1.50, 0, true],
-    ["1.1.2", "Seed Name", "Seed Description", 5, 0, 2.00, 0, true]
+    ["1.1.1.2", "Seed Name", "Seed Description", 5, 0, 1.50, 0, true],
+    ["1.1.1.2", "Seed Name", "Seed Description", 5, 0, 2.00, 0, true]
   ]], 
   ["Common Seeds", [
-    ["1.2.1", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false],
-    ["1.2.2", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false],
-    ["1.2.2", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false]
+    ["1.1.2.1", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false],
+    ["1.1.2.2", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false],
+    ["1.1.2.2", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false]
   ]],
   ["Uncommon Seeds", [
-    ["1.3.1", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false],
-    ["1.3.2", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false],
-    ["1.3.2", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false]
+    ["1.1.3.1", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false],
+    ["1.1.3.2", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false],
+    ["1.1.3.2", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false]
   ]],
   ["Rare Seeds", [
-    ["1.4.1", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false],
-    ["1.4.2", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false],
-    ["1.4.2", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false]
+    ["1.1.4.1", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false],
+    ["1.1.4.2", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false],
+    ["1.1.4.2", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false]
   ]],
 ];
 
 var Soils = [
-  ["2.1.1", "Soil Name", "Soil Description", 1, true],
-  ["2.1.1", "Soil Name", "Soil Description", 1.5, false],
-  ["2.1.1", "Soil Name", "Soil Description", 2, false]
+  ["1.2.1.1", "Soil Name", "Soil Description", 1, true],
+  ["1.2.1.2", "Soil Name", "Soil Description", 1.5, false],
+  ["1.2.1.3", "Soil Name", "Soil Description", 2, false]
 ];
 
 ////////////////////////////////
