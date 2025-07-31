59dc4a1a5cf95852f1637e5a59544ae27ec6e3cf
Olive :3 <141564336+vOlive-io@users.noreply.github.com>
2025-07-31 10:47:26 -0600

    Update ingameScript.js

--- play/ingameScript.js
+++ play/ingameScript.js
@@ -1,1 +1,25 @@
+////////////////////////////////
+///       BOOT-UP GAME       ///
+////////////////////////////////
+
+
+
+
+////////////////////////////////
+///        VARIABLES         ///
+////////////////////////////////
+
+var Seeds = [
+  ["Starter Seeds", [
+    ["1_1", "Seed Name", true],
+    ["1_2", "Seed Name", true],
+    ["1_2", "Seed Name", true]
+  ]], 
+  ["Common Seeds", [
+    ["2_1", "Seed Name", false],
+    ["2_2", "Seed Name", false],
+    ["2_2", "Seed Name", false]
+  ]]
+];
+
 
