70578be2ffd0223d0b6a7016dd6701957afcb43f
Olive :3 <141564336+vOlive-io@users.noreply.github.com>
2025-07-31 11:56:44 -0600

    fixed tabs

--- play/ingameScript.js
+++ play/ingameScript.js
@@ -14,38 +14,38 @@
 // ids * <number of item class type> . <number of item class > . <number of item subclass> . <item number>
 
 var Seeds = [
-  ["Starter Seeds", [
-    ["1.1.1.1", "Seed Name", "Seed Description", 5, 0, 1.00, 0, true],
-    ["1.1.1.2", "Seed Name", "Seed Description", 5, 0, 1.50, 0, true],
-    ["1.1.1.2", "Seed Name", "Seed Description", 5, 0, 2.00, 0, true]
-  ]], 
-  ["Common Seeds", [
-    ["1.1.2.1", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false],
-    ["1.1.2.2", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false],
-    ["1.1.2.2", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false]
-  ]],
-  ["Uncommon Seeds", [
-    ["1.1.3.1", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false],
-    ["1.1.3.2", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false],
-    ["1.1.3.2", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false]
-  ]],
-  ["Rare Seeds", [
-    ["1.1.4.1", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false],
-    ["1.1.4.2", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false],
-    ["1.1.4.2", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false]
-  ]],
+	["Starter Seeds", [
+		["1.1.1.1", "Seed Name", "Seed Description", 5, 0, 1.00, 0, true],
+		["1.1.1.2", "Seed Name", "Seed Description", 5, 0, 1.50, 0, true],
+		["1.1.1.2", "Seed Name", "Seed Description", 5, 0, 2.00, 0, true]
+	]], 
+	["Common Seeds", [
+		["1.1.2.1", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false],
+		["1.1.2.2", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false],
+		["1.1.2.2", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false]
+	]],
+	["Uncommon Seeds", [
+		["1.1.3.1", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false],
+		["1.1.3.2", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false],
+		["1.1.3.2", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false]
+	]],
+	["Rare Seeds", [
+		["1.1.4.1", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false],
+		["1.1.4.2", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false],
+		["1.1.4.2", "Seed Name", "Seed Description", 0, 0, 0.00, 0, false]
+	]],
 ];
 
 var Soils = [
-  ["1.2.1.1", "Soil Name", "Soil Description", 1, true],
-  ["1.2.1.2", "Soil Name", "Soil Description", 1.5, false],
-  ["1.2.1.3", "Soil Name", "Soil Description", 2, false]
+	["1.2.1.1", "Soil Name", "Soil Description", 1, true],
+	["1.2.1.2", "Soil Name", "Soil Description", 1.5, false],
+	["1.2.1.3", "Soil Name", "Soil Description", 2, false]
 ];
 
 //ACHEVMENTS
 var achievements = [
-  ["2.1.1.1", "Achievement Name", "Achievement Description", false],
-  ["2.1.1.2", "Achievement Name", "Achievement Description", false]
+	["2.1.1.1", "Achievement Name", "Achievement Description", false],
+	["2.1.1.2", "Achievement Name", "Achievement Description", false]
 ];
 
 ////////////////////////////////
@@ -53,22 +53,34 @@ var achievements = [
 ////////////////////////////////
 
 function unlockSeed() {
-  if (seed[1][0][7] == true) {
+	if (seed[1][0][7] == true) {
+		const achContainer = document.createElement("li");
+    
+		const ach_h1 = document.createElement("h1");
+		const ach_h1_text = document.createTextNode("Lemonaid");
+		ach_h1.appendChild(ach_h1_text);
+
+		const ach_p = document.createElement("p");
+		const ach_p_text = document.createTextNode("yum");
+		ach_p.appendChild(ach_p_text);
+
+		achContainer.appendChild(ach_h1);
+		achContainer.appendChild(ach_p);
+		document.getElementById("myList").appendChild(achContainer);
+
+    
   }
-  const achContainer = document.createElement("li");
-
-  const ach_h1 = document.createElement("h1");
-  const ach_h1_text = document.createTextNode("Lemonaid");
-  ach_h1.appendChild(ach_h1_text);
-
-  const ach_p = document.createElement("p");
-  const ach_p_text = document.createTextNode("yum");
-  ach_p.appendChild(ach_p_text);
-
-  achContainer.appendChild(ach_h1);
-  achContainer.appendChild(ach_p);
-  document.getElementById("myList").appendChild(achContainer);
+  
 }
+
+
+
+
+
+
+
+
+
 
 function unlockTemplate() {
   const achContainer = document.createElement("li");
