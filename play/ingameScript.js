6286728ac47e8e75cde25e6525329c29027826ea
Olive :3 <141564336+vOlive-io@users.noreply.github.com>
2025-08-01 12:16:14 -0600

    Update ingameScript.js

--- play/ingameScript.js
+++ play/ingameScript.js
@@ -54,7 +54,7 @@ var achievements = [
 
 
 function unlockSeed() {
-	for (let i = 1; i < seeds.length; i++) {
+	for (let i = 1; i < seeds.length - 1; i++) {
 		for (let i2 = 1; i < seeds[i].length; i2++) {
 			for (let i3 = 0; i < seeds[i2].length; i3++) {
 				if (seeds[i][i2][i3][7] == true && seeds[i][i2][i3][8] == false) {
