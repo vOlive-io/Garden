f61f7fca0ee3544c75a0910256d6a3f6af1bfc05
Olive :3 <141564336+vOlive-io@users.noreply.github.com>
2025-08-02 12:33:51 -0600

    Update ingameScript.js

--- play/ingameScript.js
+++ play/ingameScript.js
@@ -103,6 +103,7 @@ function createDropdown() {
 	const elementsToReplace = document.querySelectorAll('.seed-list');
         elementsToReplace.forEach(element => {
                 element.replaceWith(seedContainer);
+	}
 
 }
 
