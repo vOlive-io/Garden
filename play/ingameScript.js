0d456ecc2247a8304ce5b783dc1701dde14e1978
Olive :3 <141564336+vOlive-io@users.noreply.github.com>
2025-08-04 13:32:36 -0600

    Update ingameScript.js

--- play/ingameScript.js
+++ play/ingameScript.js
@@ -16,7 +16,7 @@ setInterval(changeSeason, 15000);
 //Seeds array counts plants AND seeds in inventory
 // ids * <number of item class type> . <number of item class > . <number of item subclass> . <item number>
 //["<Item ID>*", "Seed Name", "Seed Description", <seeds owned: int>, <time to grow (seasons): int>, <base value: dub>, <total planted: int>, <unlocked: bool>],
-
+var seasonNum = 0;
 
 var seeds = [
 	["Start of seeds", ["Seed Types", [
@@ -132,7 +132,6 @@ var slotNum = 4;
 ////////////////////////////////
 
 function changeSeason() {
-	let seasonNum = 0;
 	let seasonColors = ["#c2f7ab", "#ffe066","#ffc58f","#b5f1ff"];
 	const garden = document.getElementById("gardenUI");
 	const newColor = seasonColors[seasonNum];
