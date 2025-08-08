2d82ce012e0470edb2776b4614c1b1a73a14fd64
Olive :3 <141564336+vOlive-io@users.noreply.github.com>
2025-08-08 13:53:35 -0600

    Update ingameScript.js

--- play/ingameScript.js
+++ play/ingameScript.js
@@ -28,6 +28,7 @@ document.getElementById('garden-slots').addEventListener('click', function(event
 var seasonNum = 0;
 var selectedSeed = "wheat";
 var mon = 0;
+totalSeeds = 0;
 var seeds = [
 	["Start of seeds", ["Seed Types", [
 		[0, "Garden Produce", 0],
@@ -134,8 +135,21 @@ var soils = [
 
 //ACHEVMENTS
 var achievements = [
-	["2.1.1.1", "Getting Started", "Plant your first seed!", "ach_2.1.1.1", false, false],
-	["2.1.1.2", "Humble Garden", "Plant 5 seeds!", "ach_2.1.1.2", false, false]
+	["2.1.1.1", "Getting Started", "Plant your first seed!", "Achievement Description", false, false],
+	["2.1.1.2", "Microsopic Garden", "Plant 5 seeds!", "Achievement Description",false, false],
+	["2.1.1.3", "Tiny Garden", "Plant 10 seeds!", "Achievement Description",false, false],
+	["2.1.1.4", "Small Garden", "Plant 25 seeds!", "Achievement Description",false, false],
+	["2.1.1.5", "Humble Garden", "Plant 50 seeds!", "Achievement Description",false, false],
+	["2.1.1.6", "Average Garden", "Plant 100 seeds!", "Achievement Description",false, false],
+	["2.1.1.7", "Mediocer Garden", "Plant 250 seeds!", "Achievement Description",false, false],
+	["2.1.1.8", "Big Garden", "Plant 500 seeds!", "Achievement Description",false, false],
+	["2.1.1.9", "Large Garden", "Plant 1,000 seeds!", "Achievement Description",false, false],
+	["2.1.1.10", "Huge Garden", "Plant 2,500 seeds!", "Achievement Description",false, false],
+	["2.1.1.11", "Giant Garden", "Plant 5,000 seeds!", "Achievement Description",false, false],
+	["2.1.1.12", "Massive Garden", "Plant 10,000 seeds!", "Achievement Description",false, false],
+	["2.1.1.13", "Humongus Garden", "Plant 25,000 seeds!", "Achievement Description",false, false],
+	["2.1.1.14", "Enormous Garden", "Plant 50,000 seeds!", "Achievement Description",false, false],
+	["2.1.1.15", "Divine Garden", "Plant 100,000 seeds!", "Achievement Description",false, false]
 ];
 var slotNum = 4;
 
