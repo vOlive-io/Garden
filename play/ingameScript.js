8de546fd537861ee138de746bc4c6cf16fdfa961
vOlive2.0 <OLiveA148@lpsk12.org>
2025-08-15 11:03:17 -0600

    added better discriptions to plant gen

--- play/ingameScript.js
+++ play/ingameScript.js
@@ -222,7 +222,9 @@ function unlockSeed() {
 				if (seeds[i][1][i3][5] == 0) {seasonNameInput = "spring";}
 				
 				const seed_p = document.createElement("p");
-				const seed_p_text = document.createTextNode(seeds[i][1][i3][2] + "Grows slower in " + seasonNameInput + ".");
+				const p_part_1 = "\nGrows slower in " + seasonNameInput + ".";
+				const p_part_2 = "\nGrows for " + seeds[i][1][i3][6] + "$"; 
+				const seed_p_text = document.createTextNode(seeds[i][1][i3][2] + p_part_1 + p_part_2);
 				seed_p.appendChild(seed_p_text);
 				
 				seedContainer.appendChild(seed_h1);
