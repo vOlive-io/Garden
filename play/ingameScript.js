69c5c8045339e0472d59902bad112829deace607
vOlive2.0 <OLiveA148@lpsk12.org>
2025-08-21 11:11:41 -0600

    Update ingameScript.js

--- play/ingameScript.js
+++ play/ingameScript.js
@@ -3,7 +3,7 @@ callSavedData_cookies();
 ///        VARIABLES         ///
 ////////////////////////////////
 //add time 35:42.92
-//add time 7min
+//add time 7min 
 
 
 //Seeds array counts plants AND seeds in inventory
